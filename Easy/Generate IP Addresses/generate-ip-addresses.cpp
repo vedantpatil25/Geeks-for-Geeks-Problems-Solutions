96eada82286db4b5e60849fe392bd56f650f959c
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-02-14 10:47:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Generate IP Addresses/generate-ip-addresses.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this method*/
+
+class Solution{
+  public:
+    bool check(string str)
+    {
+        int n = str.length();
+        if(n<=0 || n>3)
+        {
+            return false;
+        }
+        if(str[0] == '0' && n>1)
+        {
+            return false;
+        }
+        int p = stoi(str);
+        if(p<0 || p>255)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    string generate(string s,int n,int i,int j,int k)
+    {
+        string s1 = s.substr(0,i+1);
+        string s2 = s.substr(i+1,j-i);
+        string s3 = s.substr(j+1,k-j);
+        string s4 = s.substr(k+1,n-k-1);
+        if(check(s1) && check(s2) && check(s3) && check(s4))
+        {
+            return s1+"."+s2+"."+s3+"."+s4;
+        }
+        return "";
+    }
+    vector<string> genIp(string &s) {
+        // Your code here
+        vector<string> v;
+        int n = s.length();
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                for(int k = j+1;k<n;k++)
+                {
+                    string st = generate(s,n,i,j,k);
+                    if(st.length()>0)
+                    {
+                        v.push_back(st);
+                    }
+                }
+            }
+        }
+        if(v.empty())
+        {
+            return {"-1"};
+        }
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s;
+        cin >> s;
+
+        /*generating naively*/
+        Solution obj;
+        vector<string> str = obj.genIp(s);
+        sort(str.begin(), str.end());
+        if(str.size()==0)
+            cout << -1 << "\n";
+        else{
+            for (auto &u : str) {
+                cout << u << "\n";
+            }
+        }
+    }
+}
+// } Driver Code Ends
