615940239adba568b663240fedbf4df249f5162c
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-10-17 00:35:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Stickler Thief/stickler-thief.cpp
+++ Medium/Stickler Thief/stickler-thief.cpp
@@ -8,32 +8,19 @@ class Solution
 {
     public:
     //Function to find the maximum money the thief can get.
-    
-    int solve(int ind , int *arr, vector<int>&dp)
-	{
-	    if(ind == 0 ) return arr[ind];
-	    if(ind < 0) return 0;
-	    
-	    if(dp[ind] != -1) return dp[ind];
-	    
-	    int pick = arr[ind] + solve(ind-2, arr, dp);
-	    int notPick = solve(ind-1, arr, dp);
-	    return dp[ind] = max(pick, notPick);
-	}
-	
-    int FindMaxSum(int arr[], int n)
-    {
-        // Your code here
-        vector<int> temp1,temp2;
+    int solve(int ind, int arr[], vector<int>&dp){
+        if(dp[ind] != -1) return dp[ind];
+        if(ind == 0) return arr[ind];
+        if(ind < 0) return 0;
+        int pick = solve(ind-2, arr, dp) + arr[ind];
+        int notPick = solve(ind-1, arr, dp);
         
-        for(int i =0 ; i < n ; i++)
-        {
-            if(i != 0 ) temp1.push_back(arr[i]);
-            if(i != n-1 ) temp1.push_back(arr[i]);
-        }
-        vector<int>dp1(n+1 , -1);
-        vector<int>dp2(n+1 , -1);
-	    return max( solve(n-1 , arr , dp1) ,  solve(n-1 , arr , dp2) );
+        return dp[ind] = max(pick, notPick);
+    }
+    int FindMaxSum(int arr[], int n){
+        // Your code here
+        vector<int>dp(n,-1);
+        return solve(n-1, arr, dp);
     }
 };
 
